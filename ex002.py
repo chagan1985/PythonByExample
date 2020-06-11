d53c6350a89f5bedaccf40624f8e4d6d3215789a
Christopher Hagan <christopherhagan1985@gmail.com>
2020-06-11 21:04:23 +0100

    Playing with github

---
+++ ex002.py
@@ -0,0 +1,9 @@
+#####
+# Python By Example
+# Exercise 002
+# Christopher Hagan
+#####
+
+firstName = input('What is your forename? ')
+secondName = input('And what is your surname? ')
+print('Hello {} {}'.format(firstName, secondName))
---
+++ ex003.py
@@ -0,0 +1,7 @@
+#####
+# Python By Example
+# Exercise 003
+# Christopher Hagan
+#####
+
+print('What do you call a bear with no teeth?\nA gummy bear!')
